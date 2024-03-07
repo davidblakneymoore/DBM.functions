21613c706ebeef7124ea68ce83451bf38e39323e
David Moore <46967676+davidblakneymoore@users.noreply.github.com>
2024-03-07 18:41:50 -0500

    Add files via upload

---
+++ R/data-Made_Up_Data_for_the_Aligning_Values_Across_Multiple_Vertical_Axes_Function.R
@@ -0,0 +1,15 @@
+#' Made-up Data for the 'Aligning_Values_Across_Multiple_Vertical_Axes' Function
+#'
+#' This data frame contains made-up data to use in the example in the documentation of the 'Aligning_Values_Across_Multiple_Vertical_Axes' function.
+#'
+#' @format `Made_Up_Data_for_the_Aligning_Values_Across_Multiple_Vertical_Axes_Function` is a `data.frame` with 100 rows and 4 columns.
+#' \describe{
+#' \item{Index}{An indexing variable}
+#' \item{Variable_1, Variable_2, Variable_3}{Variables (each of which is measured in different units) that are to be aligned across vertical axes on a graph}
+#' }
+#' 
+#' @author David B. Moore (\email{davidblakneymoore@@gmail.com})
+#' 
+#' @examples
+#' Made_Up_Data_for_the_Aligning_Values_Across_Multiple_Vertical_Axes_Function
+"Made_Up_Data_for_the_Aligning_Values_Across_Multiple_Vertical_Axes_Function"
