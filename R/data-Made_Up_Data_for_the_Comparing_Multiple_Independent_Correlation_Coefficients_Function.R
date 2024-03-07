4656280980e38e70a8cd917f446bdaa54de721b8
David Moore <46967676+davidblakneymoore@users.noreply.github.com>
2024-03-07 18:38:31 -0500

    Add files via upload

---
+++ R/data-Made_Up_Data_for_the_Comparing_Multiple_Independent_Correlation_Coefficients_Function.R
@@ -0,0 +1,14 @@
+#' Made-up Data for the 'Comparing_Multiple_Independent_Correlation_Coefficients' Function
+#'
+#' This data frame contains made-up data to use in the example in the documentation of the 'Comparing_Multiple_Independent_Correlation_Coefficients' function.
+#'
+#' @format `Made_Up_Data_for_the_Comparing_Multiple_Independent_Correlation_Coefficients_Function` is a `data.frame` with 5 rows and 3 columns.
+#' \describe{
+#' \item{Name}{The name of the correlation}
+#' \item{Coefficient_of_Correlation}{The correlation coefficient}
+#' \item{Sample_Size}{The number of observations that went into calculating the correlation coefficient}
+#' }
+#' 
+#' @examples
+#' Made_Up_Data_for_the_Comparing_Multiple_Independent_Correlation_Coefficients_Function
+"Made_Up_Data_for_the_Comparing_Multiple_Independent_Correlation_Coefficients_Function"
